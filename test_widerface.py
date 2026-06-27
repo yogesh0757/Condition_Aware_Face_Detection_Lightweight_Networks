bc843813b84a2e0de817caf2c9de099bb1f4d645
yogesh0757 <77352269+yogesh0757@users.noreply.github.com>
2026-06-27 17:39:47 +0530

    Update test_widerface.py

--- test_widerface.py
+++ test_widerface.py
@@ -15,9 +15,9 @@ from ptflops import get_model_complexity_info as cp
 
 
 parser = argparse.ArgumentParser(description='LWFD')
-parser.add_argument('-m', '--trained_model', default='./weights/WO_CAFACLite_SV2.pth',
+parser.add_argument('-m', '--trained_model', default='./weights/CAFACLite_BV4.pth',
                     type=str, help='Trained state_dict file path to open')
-parser.add_argument('--network', default='shufflenet_v2_x0_5', help='Backbone network BBLiteV4, mobilenet0.25 or shufflenet_v2_x0_5')
+parser.add_argument('--network', default='BBLiteV4', help='Backbone network BBLiteV4, mobilenet0.25 or shufflenet_v2_x0_5')
 parser.add_argument('--origin_size', default=False, type=str, help='Whether use origin image size to evaluate')
 parser.add_argument('--save_folder', default='./widerface_evaluate/widerface_txt/', type=str, help='Dir to save txt results')
 parser.add_argument('--cpu', action="store_true", default=False, help='Use cpu inference')
